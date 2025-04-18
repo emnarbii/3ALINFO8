7997dd555e03900384464c2559713c0ead89558d
emnarbii <emna.rbii@esprit.tn>
2025-04-18 12:19:04 +0100

    services

--- src/app/residence-form/residence-form.component.ts
+++ src/app/residence-form/residence-form.component.ts
@@ -4,26 +4,27 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-residence-form',
   templateUrl: './residence-form.component.html',
-  styleUrls: ['./residence-form.component.css']
+  styleUrls: ['./residence-form.component.css'],
 })
 export class ResidenceFormComponent {
+  residenceForm!: FormGroup;
 
-  residenceForm!:FormGroup;
+  constructor() {
+    this.residenceForm = new FormGroup({
+      id: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.minLength(3),Validators.maxLength(15)]),
+      adress: new FormControl('', Validators.required),
+      image: new FormControl('', [Validators.required, Validators.pattern("\.(.jpg|.svg)$")]),
+      status: new FormControl('', Validators.required),
+    });
+  }
 
-constructor(){
-  this.residenceForm= new FormGroup({
-    id: new FormControl('',Validators.required),
-    name: new FormControl('',Validators.required),
-    adress: new FormControl('',Validators.required),
-    image: new FormControl('',Validators.required),
-    status: new FormControl('',Validators.required)
-  } 
+  getFormValue() {
+    // return console.log(this.residenceForm.value);
+    return console.log(this.residenceForm.get('name')?.value)
+  }
 
-  )
-}
- 
-getFormValue(){
-  
-  return console.log(this.residenceForm.value)
-}
+  get name(){
+    return this.residenceForm.get('name')
+  }
 }
